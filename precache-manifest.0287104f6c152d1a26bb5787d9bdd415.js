2b267454b104f15f8e8affc771ada8dd9115c920
AbhishekDikshit <abhishekdikshit@outlook.com>
2020-04-16 09:32:45 +0530

    Updates

---
+++ precache-manifest.0287104f6c152d1a26bb5787d9bdd415.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "03a80a45c48b29e7aa9a",
+    "url": "/AbhishekDikshit69/React-Colors-App/static/css/main.df3b3ddb.chunk.css"
+  },
+  {
+    "revision": "03a80a45c48b29e7aa9a",
+    "url": "/AbhishekDikshit69/React-Colors-App/static/js/main.5ff9aea2.chunk.js"
+  },
+  {
+    "revision": "fe60be0d1667dd43ab06",
+    "url": "/AbhishekDikshit69/React-Colors-App/static/js/runtime~main.06cb1e83.js"
+  },
+  {
+    "revision": "9d3dc1d13d4898dd973d",
+    "url": "/AbhishekDikshit69/React-Colors-App/static/css/2.20cc5add.chunk.css"
+  },
+  {
+    "revision": "9d3dc1d13d4898dd973d",
+    "url": "/AbhishekDikshit69/React-Colors-App/static/js/2.c439ba46.chunk.js"
+  },
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/AbhishekDikshit69/React-Colors-App/static/media/bg.77428cd4.svg"
+  },
+  {
+    "revision": "0f6f198d8a63c1594fb6bf95e78e5784",
+    "url": "/AbhishekDikshit69/React-Colors-App/index.html"
+  }
+];
